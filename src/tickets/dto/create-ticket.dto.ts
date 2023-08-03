94919f5a1c0f0ac13f7335a2836ae1fd9a1c14ca
1tpp <panapat.dev@gmail.com>
2023-08-03 21:34:12 +0700

    refactor: ticker interface

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -1,11 +1,11 @@
 import { IProvider } from '@/common/interfaces/provider.interface';
-import { ILocation } from '@/common/interfaces/location.interface';
+// import { ILocation } from '@/common/interfaces/location.interface';
 export class CreateTicketDto {
   title: string;
   description: string;
-  depart_location: string;
+  depart_location: string; // TODO: change to ILocation
   // depart_location: ILocation;
-  arrive_location: string;
+  arrive_location: string; // TODO: change to ILocation
   // arrive_location: ILocation;
   date: string;
   depart_time: string;
