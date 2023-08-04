44d02a00e9a6139bff7ddbf854ddf35aab1acab7
TeetouchQQ <teawkung@live.com>
2023-08-04 13:14:52 +0700

    feat: update create project dto

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -3,7 +3,9 @@ import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { Project,ProjectDocument } from './schemas/project.schema';
 import { Model } from 'mongoose';
+import { BuyProjectDto } from './dto/buy-project.dto';
 import {InjectModel} from "@nestjs/mongoose";
+import { Imember } from '@/common/interfaces/member.interface';
 @Injectable()
 export class ProjectsService {
   constructor(
@@ -19,6 +21,44 @@ export class ProjectsService {
 
   async findAll() {
     return await this.projectModel.find().exec();
+  }
+
+
+  async buy(buyProjectDto:BuyProjectDto,user) {
+    const project = await this.projectModel.findById(buyProjectDto.id).exec();
+    
+    const maximum = project.maximum;
+
+    if (project.amount + buyProjectDto.amount > project.maximum) {
+      throw new Error('maximum amount exceeded');
+    }else{
+      const customer = await project.member.find((member) => {
+        return member.user == user._id;
+      });
+      if (customer) {
+        customer.amount += buyProjectDto.amount;
+      } else {
+        let to_add:Imember = {
+          user: user._id,
+          amount: buyProjectDto.amount,
+          lastbuy: Date.now() as unknown as Date,
+          percentage: 0,
+        } 
+        // project.member.push({
+          
+        // });
+      project.amount += buyProjectDto.amount;
+      project.save();
+    }
+
+
+    project.member.push(user._id);
+    await project.save();
+  }
+  }
+
+  async findMember(id: string) {
+
   }
 
   findOne(id: number) {
