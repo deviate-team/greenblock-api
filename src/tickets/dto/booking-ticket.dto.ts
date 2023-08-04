fbdfecbcbd4bb9d53cd3e5c29e01144dea0f6bc5
1tpp <panapat.dev@gmail.com>
2023-08-04 22:16:47 +0700

    chore: update dto

--- src/tickets/dto/booking-ticket.dto.ts
+++ src/tickets/dto/booking-ticket.dto.ts
@@ -1,11 +1,19 @@
 import { IsNotEmpty, IsNumber, IsEnum } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { TicketOption } from '@/common/enums/ticket-option.enum';
-
 export class BookingTicketDto {
+  @ApiProperty({
+    description: 'Quantity',
+    example: 20,
+  })
   @IsNotEmpty()
   @IsNumber()
   quantity: number;
 
+  @ApiProperty({
+    description: 'Option',
+    example: TicketOption.Standard,
+  })
   @IsNotEmpty()
   @IsEnum(TicketOption)
   option: TicketOption;
