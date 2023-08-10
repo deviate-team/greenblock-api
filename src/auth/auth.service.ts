122944a9f12a3709939608a4d443e1a5e79259eb
1tpp <panapat.dev@gmail.com>
2023-08-10 07:24:23 +0700

    chore: hack

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,104 +31,112 @@ export class AuthService {
   }
 
   async signIn(signInDto: SignInDto): Promise<string> {
-    const { email, password } = signInDto;
+    try {
+      const { email, password } = signInDto;
 
-    const userExists = await this.userModel.findOne({ email }).exec();
-    if (!userExists) {
-      throw new HttpException(
-        {
-          success: false,
-          message: 'User not found',
-        },
-        404,
+      const userExists = await this.userModel.findOne({ email }).exec();
+      if (!userExists) {
+        throw new HttpException(
+          {
+            success: false,
+            message: 'User not found',
+          },
+          404,
+        );
+      }
+
+      const passwordMatch = await bcrypt.compareSync(
+        password,
+        userExists.password,
       );
-    }
+      if (!passwordMatch) {
+        throw new HttpException(
+          {
+            success: false,
+            message: 'Invalid credentials',
+          },
+          401,
+        );
+      }
 
-    const passwordMatch = await bcrypt.compareSync(
-      password,
-      userExists.password,
-    );
-    if (!passwordMatch) {
-      throw new HttpException(
-        {
-          success: false,
-          message: 'Invalid credentials',
-        },
-        401,
-      );
+      return await this.generateToken({
+        userId: userExists._id,
+        role: userExists.role,
+      });
+    } catch (error) {
+      console.log(error);
     }
-
-    return await this.generateToken({
-      userId: userExists._id,
-      role: userExists.role,
-    });
   }
 
   async signUp(signUpDto: SignUpDto) {
-    const { email, username, password, confirmPassword, phoneNumber, role } =
-      signUpDto;
-    if (role == 'admin') {
-      throw new HttpException(
-        {
-          success: false,
-          message: 'You can not register as admin',
-        },
-        400,
-      );
-    }
-    const role_enum = Object.values(Role);
-    if (!role_enum.includes(role as string as unknown as Role)) {
-      throw new HttpException(
-        {
-          success: false,
-          message: 'Role not found',
-        },
-        400,
-      );
-    }
+    try {
+      const { email, username, password, confirmPassword, phoneNumber, role } =
+        signUpDto;
+      if (role == 'admin') {
+        throw new HttpException(
+          {
+            success: false,
+            message: 'You can not register as admin',
+          },
+          400,
+        );
+      }
+      const role_enum = Object.values(Role);
+      if (!role_enum.includes(role as string as unknown as Role)) {
+        throw new HttpException(
+          {
+            success: false,
+            message: 'Role not found',
+          },
+          400,
+        );
+      }
 
-    if (password !== confirmPassword) {
-      throw new HttpException(
-        {
-          success: false,
-          message: 'Passwords do not match',
-        },
-        400,
-      );
+      if (password !== confirmPassword) {
+        throw new HttpException(
+          {
+            success: false,
+            message: 'Passwords do not match',
+          },
+          400,
+        );
+      }
+
+      if (!phoneNumber) {
+        throw new HttpException(
+          {
+            success: false,
+            message: 'Phone number is required',
+          },
+          400,
+        );
+      }
+
+      const userExists = await this.userModel.find({
+        $or: [{ email }, { username }],
+      });
+
+      if (userExists.length) {
+        throw new HttpException(
+          {
+            success: false,
+            message: 'Username or email already exists',
+          },
+          409,
+        );
+      }
+      const salt = await bcrypt.genSalt();
+      const newUser = await this.userModel.create({
+        ...signUpDto,
+        password: await bcrypt.hashSync(password, salt),
+      });
+
+      return await this.generateToken({
+        userId: newUser._id,
+        role: newUser.role,
+      });
+    } catch (error) {
+      console.log(error);
     }
-
-    if (!phoneNumber) {
-      throw new HttpException(
-        {
-          success: false,
-          message: 'Phone number is required',
-        },
-        400,
-      );
-    }
-
-    const userExists = await this.userModel.find({
-      $or: [{ email }, { username }],
-    });
-
-    if (userExists.length) {
-      throw new HttpException(
-        {
-          success: false,
-          message: 'Username or email already exists',
-        },
-        409,
-      );
-    }
-    const salt = await bcrypt.genSalt();
-    const newUser = await this.userModel.create({
-      ...signUpDto,
-      password: await bcrypt.hashSync(password, salt),
-    });
-
-    return await this.generateToken({
-      userId: newUser._id,
-      role: newUser.role,
-    });
   }
 }
